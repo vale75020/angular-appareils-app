c16e896f2bdc43e9baac2c87ffc633b68a3e7026
vale75020 <valentinosuma@hotmail.com>
2019-10-02 10:53:26 +0200

    input

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  isAuth = false;
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true
+      }, 4000
+    );
+  }
+  onOn() {
+    console.log("every appareil is ON")
+  }
+}
+
+
---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-appareil",
+  templateUrl: "./appareil.component.html",
+  styleUrls: ["./appareil.component.scss"]
+})
+export class AppareilComponent implements OnInit {
+  appareilName = "Washing Machine";
+  appareilStatus = "Off";
+  constructor() {}
+
+  ngOnInit() {}
+
+  getStatus() {
+    return this.appareilStatus
+  }
+}
